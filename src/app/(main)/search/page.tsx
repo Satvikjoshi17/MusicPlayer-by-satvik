2c835f3c0cfb1a52cb3c104d39ec92023f4278ed
Satvik joshi_SJ_ <satvikjoshi9thc@gmail.com>
2025-09-23 17:50:30 +0000

    useSearchParams() should be wrapped in a suspense boundary at page "/sea

--- src/app/(main)/search/page.tsx
+++ src/app/(main)/search/page.tsx
@@ -1,7 +1,11 @@
 import { SearchClientPage } from '@/components/music/search-client-page';
+import { TrackListSkeleton } from '@/components/music/track-list-skeleton';
+import { Suspense } from 'react';
 
 export default function SearchPage() {
   return (
-    <SearchClientPage />
+    <Suspense fallback={<TrackListSkeleton count={5} />}>
+      <SearchClientPage />
+    </Suspense>
   );
 }
